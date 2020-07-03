723b221e16c6a5978887880586c2fe56996aa9c2
rudy-prietno <prietnorudy@gmail.com>
2020-07-03 17:15:53 +0700

    first

--- DMP/commandl/command.py
+++ DMP/commandl/command.py
@@ -8,15 +8,3 @@ class commandline:
     def execute(self):
         s = subprocess.getstatusoutput(self.statement)
         print(s[1])
-
-class commandexec:
-    def __init__(self, statement, num):
-        self.statement = statement
-        self.num = num
-
-    def exec_container(self):
-        # Reference the command into docker container
-        client = docker.from_env()
-        p2=client.containers.list()
-        display(p2[self.num].exec_run(self.statement, stdout=True, stderr=True))
-
